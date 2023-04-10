37446b89ffad752253993799bbe4f8082780da74
Vladgobelen <vladgobelen@gmail.com>
2023-04-10 09:48:35 +0700

    замена hsh

--- achiv.lua
+++ achiv.lua
@@ -19,33 +19,45 @@ function mysplit (inputstr, sep)
 	return t
 end
 
+function alfabet (bookv)
+shablon="абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	myB=string.find(shablon,bookv)
+	return myB
+end
+
 function hashStr (nome)
-	hours, minutes = GetGameTime()
-	count1=hours* 3.1415926535
-	count2=minutes* 3.1415926535
-	count3=count1*count2
-	count3=string.sub(count3, 1, 3)
-	count3=string.format("%03d",count3)
-	hNik=string.byte(nome,1)
-	hNik2=string.byte(nome,2)
-	hNome=hNik*hNik2
+	i = time()
+	i = string.sub(i, 8, 9)
+	i = i * i
+	i = i * 3.1415926535
+	i = string.sub(i, 3, 5)
+	i = string.format("%03d",i)
+	nome1=string.sub(nome, 1, 1)
+	nome2=string.sub(nome, 2, 2)
+	nome1=alfabet(nome1)
+	nome2=alfabet(nome2)
+	hNome=nome1*nome2
 	hNome=string.sub(hNome, 1, 3)
 	hNome=string.format("%03d",hNome)
-	r1=string.sub(count3, 1, 1)
+	r1=string.sub(i, 1, 1)
 	r2=string.sub(hNome, 1, 1)
-	r3=string.sub(count3, 2, 2)
+	r3=string.sub(i, 2, 2)
 	r4=string.sub(hNome, 2, 2)
-	r5=string.sub(count3, 3, 3)
+	r5=string.sub(i, 3, 3)
 	r6=string.sub(hNome, 3, 3)
 	r=r1 .. r2 .. r3 .. r4 .. r5 .. r6
 	return r
 end
+--
+local myNome1 = GetUnitName("player")
+testNome=hashStr(myNome1)
+print (myNome1 .. " " .. testNome)
+--
 local myNome = GetUnitName("player")
 hsh=hashStr(myNome)
 hshStraniero=hashStr(sender)
 local nachalo = string.sub(message, 1, 1)
 
-if sender == "Витинари" or sender == "Детрит" or sender == "Железобетонс" or sender == "Двацветок" then
 	if string.find (message, "#aaa") and string.find(message, hsh) and nachalo~="*" then
 		msg1 = mysplit(message)
 		msg1 = msg1[7]
@@ -57,7 +69,10 @@ if sender == "Витинари" or sender == "Детрит" or sender == "Жел
         	SendChatMessage(hsh .. " #aac " .. "можно сделать: " .. msg1 .. " " .. GetAchievementLink(msg1), "OFFICER", nil, 1)
 		end
 	end
+print (hsh)
+
 if string.find (message, hsh) and string.find (message, "#aaf") then
+print ("должно же работать")
 	proverka_komandy=mysplit(message)
 	msg1=proverka_komandy[8]
 	msg1 = tonumber(msg1)
@@ -71,7 +86,7 @@ if string.find (message, hsh) and string.find (message, "#aaf") then
 end
 
 
-end
+
 
 end
 )
