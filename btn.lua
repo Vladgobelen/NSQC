944746fa12a93d68a9d50c32add000baf743a5ff
Vladgobelen <vladgobelen@gmail.com>
2023-04-16 15:59:00 +0700

    кнопка для проверки текущего квеста. Все еще нужны тесты

--- btn.lua
+++ btn.lua
@@ -23,6 +23,12 @@ function btn:configure(id,posex,posey,sizex,sizey,zzid,message)
             testQ["бонусный_квест"]=9999
             end)
         end
+        if id==8 then
+            self[id]:SetScript("OnClick",function(self, button)
+                SendChatMessage(GetAchievementLink(testQ["взятый_квест"]), "GUILD", nil, 1)
+
+                end)
+        end
     end
 end
 
@@ -34,8 +40,9 @@ btn:configure(2,0,370,300,30,"#zzr","Сдать квест");
 btn:configure(3,0,340,300,30,"#zzz","Взять бонусный квест вне лимита");
 btn:configure(4,0,310,300,30,"#zzy","Сдать бонусный квест вне лимита");
 btn:configure(5,0,280,300,30,"#zzt","Узнать свой гилдлвл");
-btn:configure(8,-400,400,100,30,"","тест");
-btn:configure(7,0,250,300,30,"#zzp","Отказаться от квеста");
+btn:configure(9,-400,400,100,30,"","тест");
+btn:configure(7,0,220,300,30,"#zzp","Отказаться от квеста");
+btn:configure(8,0,250,300,30,"","Узнать текущий квест");
 
 
 
@@ -82,12 +89,12 @@ minibtn:SetScript("OnClick", function()
     if pokazat~=1 then
         ii=6
         btn[ii] = CreateFrame("Button", nil, UIParent, "UIPanelButtonTemplate")
-        btn[ii]:SetPoint("CENTER",0,220)
+        btn[ii]:SetPoint("CENTER",0,190)
         btn[ii]:SetSize(300, 30)
         btn[ii]:SetText("Закрыть")
         btn[ii]:Hide();
         btn[ii]:SetScript("OnClick", function(self, button)
-        for ii=1,7 do
+        for ii=1,8 do
             btn[ii]:Hide();
         end
         pokazat=0
@@ -95,7 +102,7 @@ minibtn:SetScript("OnClick", function()
         minibtn:SetPushedTexture("Interface/COMMON/Indicator-Red.png")
         minibtn:SetHighlightTexture("Interface/COMMON/Indicator-Red.png")
         end)
-        for ii=1,7 do
+        for ii=1,8 do
             btn[ii]:Show();
         end
         pokazat=1
@@ -103,7 +110,7 @@ minibtn:SetScript("OnClick", function()
         minibtn:SetPushedTexture("Interface/COMMON/Indicator-Green.png")
         minibtn:SetHighlightTexture("Interface/COMMON/Indicator-Green.png")
     else
-        for ii=1,7 do
+        for ii=1,8 do
             btn[ii]:Hide();
         end
         minibtn:SetNormalTexture("Interface/COMMON/Indicator-Red.png")
@@ -118,7 +125,7 @@ end)
 if framePos==nil then
     framePos={}
 end
-btn[8]:SetScript("OnClick",function(self, button)
+btn[9]:SetScript("OnClick",function(self, button)
 if setPos==1 or setPos==nil then
     setPos=0
 
@@ -139,15 +146,15 @@ GC_Sniffer111:SetScript("OnEvent", function (self, event, message, sender)
 local myNome = GetUnitName("player")
 if sender==myNome and message=="stop" then
     framePos[1]=0
-    btn[8]:Disable()
+    btn[9]:Disable()
 elseif sender==myNome and message=="start" then
     framePos[1]=1
-    btn[8]:Enable()
+    btn[9]:Enable()
 elseif sender==myNome and message=="скрыть" then
-    btn[8]:Hide()
+    btn[9]:Hide()
 elseif sender==myNome and message=="показать" then
-    btn[8]:Show()
-    btn[8]:Disable()
+    btn[9]:Show()
+    btn[9]:Disable()
 end
 
 
@@ -185,6 +192,14 @@ frameTime:HookScript("OnUpdate", function(self, elapsed)
             btn[2]:SetText("Сдать квест")
         end
     end
+
+    if testQ["взятый_квест"]==nil or testQ["взятый_квест"]=="9999" then
+                btn[8]:Disable();
+                btn[9]:SetText("Нет взятых квестов")
+            else
+                btn[8]:SetText("Узнать текущий квест")
+                btn[8]:Enable()
+            end
 
     if testQ["бонусный_квест"]==nil or testQ["бонусный_квест"]==9999 then
         btn[3]:Enable()
@@ -234,11 +249,11 @@ frameTime:HookScript("OnUpdate", function(self, elapsed)
             posmioXN, posmioYN = GetPlayerMapPosition("player");
             mioCel1=sqrt((nXres-posmioXN)^2+(nYres-posmioYN)^2)
             if mioCel > mioCel1 then
-                btn[8]:SetText("тепло")
-                btn[8]:Enable()
+                btn[9]:SetText("тепло")
+                btn[9]:Enable()
             else
-                btn[8]:SetText("холодно")
-                btn[8]:Disable()
+                btn[9]:SetText("холодно")
+                btn[9]:Disable()
             end
             mioTime=1
         end
