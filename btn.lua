85698f4149be0c4092c59bf2be70c3060d34d713
Vladgobelen <vladgobelen@gmail.com>
2023-04-20 22:54:17 +0700

    починка закрыть кнопки

--- btn.lua
+++ btn.lua
@@ -1,6 +1,6 @@
 Astrolabe = DongleStub("Astrolabe-0.4")
 Astrolabe.MinimapUpdateTime = 0.1
-versAdd=34
+versAdd=35
 local myNome = GetUnitName("player")
 ChatFrame1:AddMessage("NSQC: Клик левой кнопкой: показать аддон/скрыть аддон");
 ChatFrame1:AddMessage("NSQC: Клик правой кнопкой: показать информацию");
@@ -144,9 +144,7 @@ minibtn:SetScript("OnClick", function()
         print ("!заметка [текст заметки] - добавть информацию о себе")
         print ("!заметка+ [текст заметки] - дополнить информацию о себе")
         print ("В гильдчат: " .. myNome .. " покажи предмет [название предмета]")
-        for ii=1,8 do
-                btn[ii]:Show();
-        end
+
     end
 end)
 
