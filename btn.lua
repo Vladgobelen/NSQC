535b5bdb4ce6d254d98c451f62df5a7ada7b8d88
Vladgobelen <vladgobelen@gmail.com>
2023-04-21 20:21:38 +0700

    cтартовая реализация починки идентификации

--- btn.lua
+++ btn.lua
@@ -1,6 +1,6 @@
 Astrolabe = DongleStub("Astrolabe-0.4")
 Astrolabe.MinimapUpdateTime = 0.1
-versAdd=37
+versAdd=38
 local myNome = GetUnitName("player")
 ChatFrame1:AddMessage("NSQC: Клик левой кнопкой: показать аддон/скрыть аддон");
 ChatFrame1:AddMessage("NSQC: Клик правой кнопкой: показать информацию");
