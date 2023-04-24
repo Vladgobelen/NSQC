f30f3788b66058b433ca87f4def657d2073965e5
Vladgobelen <vladgobelen@gmail.com>
2023-04-24 17:53:23 +0700

    ролл1

--- roll.lua
+++ roll.lua
@@ -2,10 +2,9 @@ local GC_Sniffer = CreateFrame("Frame")
 GC_Sniffer:RegisterEvent("CHAT_MSG_SYSTEM")
 GC_Sniffer:SetScript("OnEvent", function (self, event, message, sender)
 local myNome = GetUnitName("player")
-
-local msg = string.gsub(message, "%s+", "")
-
-    if string.find (message, "выбрасывает") and sender==myNome then
+    rollSender=mysplit(message)
+    rollSender=rollSender[1]
+    if string.find (message, "выбрасывает") and rollSender==myNome then
        SendChatMessage(message, "OFFICER", nil, 1);
     else
     end
