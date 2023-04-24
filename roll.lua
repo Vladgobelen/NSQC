279e076c5aa9119c7de1ea759df39eb4c6c25129
Vladgobelen <vladgobelen@gmail.com>
2023-04-25 00:15:29 +0700

    настройки

--- roll.lua
+++ roll.lua
@@ -2,6 +2,9 @@ local GC_Sniffer = CreateFrame("Frame")
 GC_Sniffer:RegisterEvent("CHAT_MSG_SYSTEM")
 GC_Sniffer:SetScript("OnEvent", function (self, event, message, sender)
 local myNome = GetUnitName("player")
+    if testQ[myNome]["настройки"]==nil then
+        testQ[myNome]["настройки"]={}
+    end
     rollSender=mysplit(message)
     rollSender=rollSender[1]
     if testQ[myNome]["настройки"]["roll"]=="Enable" then
