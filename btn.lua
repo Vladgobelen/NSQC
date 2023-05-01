7d347a51ccb87f3301af3180f58352fcd16f8207
Vladgobelen <vladgobelen@gmail.com>
2023-05-01 13:04:19 +0700

    добавлен нпцскан.Скрытие текста

--- btn.lua
+++ btn.lua
@@ -1,4 +1,4 @@
-versAdd=75
+versAdd=76
 local myNome = GetUnitName("player")
 ChatFrame1:AddMessage("NSQC: Клик левой кнопкой: показать аддон/скрыть аддон");
 ChatFrame1:AddMessage("NSQC: Клик правой кнопкой: показать информацию");
