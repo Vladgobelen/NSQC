9aa00cf1390c633ddaecf8e7c96c5264db1c1d5f
Vladgobelen <vladgobelen@gmail.com>
2023-05-05 18:46:40 +0700

    тест ключей14

--- btn.lua
+++ btn.lua
@@ -1,4 +1,4 @@
-versAdd=91
+versAdd=92
 local myNome = GetUnitName("player")
 ChatFrame1:AddMessage("NSQC: Клик левой кнопкой: показать аддон/скрыть аддон");
 ChatFrame1:AddMessage("NSQC: Клик правой кнопкой: показать информацию");
