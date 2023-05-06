a6b98915a74afdff512a1d0b59afade1b4ac84d0
Vladgobelen <vladgobelen@gmail.com>
2023-05-06 17:45:31 +0700

    проверка блядских типов

--- achivC.lua
+++ achivC.lua
@@ -56,7 +56,13 @@ if string.find (message, "#aaf") and string.find (message, myNome) then
 		proverka_komandy=mysplit(message)
 		msg1=proverka_komandy[3]
 		msg1=tonumber(msg1)
-		mozhnoLiSdatChislo11=testQ[myNome]["квест_лвл2"][msg1]
+		if testQ[myNome]["квест_лвл2"][msg1] ~= nil then
+			mozhnoLiSdatChislo11=testQ[myNome]["квест_лвл2"][msg1]
+		else
+			msg1=tostring(msg1)
+			mozhnoLiSdatChislo11=testQ[myNome]["квест_лвл2"][msg1]
+		end
+		msg1=tonumber(msg1)
 		mozhnoLiSdatChislo11=tonumber(mozhnoLiSdatChislo11)
 		j=0
 		k=0
--- btn.lua
+++ btn.lua
@@ -1,4 +1,4 @@
-versAdd=95
+versAdd=96
 local myNome = GetUnitName("player")
 ChatFrame1:AddMessage("NSQC: Клик левой кнопкой: показать аддон/скрыть аддон");
 ChatFrame1:AddMessage("NSQC: Клик правой кнопкой: показать информацию");
