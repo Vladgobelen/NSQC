f1fdadb2a3caf2703edf57e020def9b63a2ebb35
Vladgobelen <vladgobelen@gmail.com>
2023-05-12 23:50:00 +0700

    Некроситет

--- btn.lua
+++ btn.lua
@@ -1,4 +1,4 @@
-versAdd=106
+versAdd=107
 local myNome = GetUnitName("player")
 ChatFrame1:AddMessage("NSQC: Клик левой кнопкой: показать аддон/скрыть аддон");
 ChatFrame1:AddMessage("NSQC: Клик правой кнопкой: показать информацию");
