a887b54c44c729f49960d576d0cf359ee7039884
Vladgobelen <vladgobelen@gmail.com>
2023-05-20 21:39:51 +0700

    новые инсты для ключей. Хер знает работает ли

--- btn.lua
+++ btn.lua
@@ -1,4 +1,4 @@
-versAdd=112
+versAdd=113
 local myNome = GetUnitName("player")
 ChatFrame1:AddMessage("NSQC: Клик левой кнопкой: показать аддон/скрыть аддон");
 ChatFrame1:AddMessage("NSQC: Клик правой кнопкой: показать информацию");
