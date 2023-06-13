ecc4bb23d08d9710a638ff7efb8a37f7f1c1b494
Vladgobelen <vladgobelen@gmail.com>
2023-06-14 02:05:00 +0700

    сиськи

--- btn.lua
+++ btn.lua
@@ -1,4 +1,4 @@
-versAdd=120
+versAdd=121
 local myNome = GetUnitName("player")
 ChatFrame1:AddMessage("NSQC: Клик левой кнопкой: показать аддон/скрыть аддон");
 ChatFrame1:AddMessage("NSQC: Клик правой кнопкой: показать информацию");
