aee70f99c8c66457fab2d03d3846aa1de41c0b24
Vladgobelen <vladgobelen@gmail.com>
2023-07-28 04:13:57 +0700

    ин квест проверка на отсутствие таблиц в эмоциях

--- Incarichi/incEm.lua
+++ Incarichi/incEm.lua
@@ -16,16 +16,21 @@ local myNome = GetUnitName("player")
 local hshStran3
 testGM=gmTest(sender)
 hshStran3=hshSenderNomeC(myNome)
-	if inQuest["бухло"]["старт"] == 1 then
-		if sender == myNome then
-			x=UnitName("target")
-			if x == "Тралл" and string.find (message, "ты меня уважаешь") then
-				SendChatMessage("Чот мне показалось, что Тралл меня не уважает...", "OFFICER", nil, 1)
-				SendChatMessage("Нужно убедиться, что я прав и крут: пойти и поцеловать Сильвану...Хотя бы издалека..", "OFFICER", nil, 1)
-				SendAddonMessage("NSGadd", "#buhloXXX", "guild")
-				inQuest["бухло"]["старт"] = 2
+	if inQuest ~= nil then
+		if inQuest["бухло"] ~= nil then
+			if inQuest["бухло"]["старт"] == 1 then
+				if sender == myNome then
+					x=UnitName("target")
+					if x == "Тралл" and string.find (message, "ты меня уважаешь") then
+						SendChatMessage("Чот мне показалось, что Тралл меня не уважает...", "OFFICER", nil, 1)
+						SendChatMessage("Нужно убедиться, что я прав и крут: пойти и поцеловать Сильвану...Хотя бы издалека..", "OFFICER", nil, 1)
+						SendAddonMessage("NSGadd", "#buhloXXX", "guild")
+						inQuest["бухло"]["старт"] = 2
+					end
+				end
 			end
 		end
 	end
+
 
 end)
--- btn.lua
+++ btn.lua
@@ -1,4 +1,4 @@
-versAdd=189
+versAdd=190
 bonusQuestF = 20
 local myNome = GetUnitName("player")
 ChatFrame1:AddMessage("NSQC: Клик левой кнопкой: показать аддон/скрыть аддон");
