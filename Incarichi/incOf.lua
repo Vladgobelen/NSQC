51589919c1a8e5366a991da1e8b320dcb14d9ae0
Vladgobelen <vladgobelen@gmail.com>
2023-08-18 04:19:31 +0700

    Квест на предметы2

--- Incarichi/incOf.lua
+++
@@ -1,9 +0,0 @@
-local GC_Sniffer = CreateFrame("Frame")
-GC_Sniffer:RegisterEvent("CHAT_MSG_OFFICER")
-GC_Sniffer:SetScript("OnEvent", function (self, event, message, sender)
-local myNome = GetUnitName("player")
-testGM=gmTest(sender)
-
-
-end
-)
