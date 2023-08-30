cbecc39709053e7de05fb1596946847ba0de22f4
Vladgobelen <vladgobelen@gmail.com>
2023-08-31 04:28:55 +0700

    добавление отображенеия памяти

--- btn.lua
+++ btn.lua
@@ -1,5 +1,5 @@
 versAdd=262
-bonusQuestF =30
+bonusQuestF = 30
 local myNome = GetUnitName("player")
 ChatFrame1:AddMessage("NSQC: Клик левой кнопкой: показать аддон/скрыть аддон");
 ChatFrame1:AddMessage("NSQC: Клик правой кнопкой: показать информацию");
