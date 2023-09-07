af00e2c412231dda8064d1070bfe733664d4e08e
Vladgobelen <vladgobelen@gmail.com>
2023-09-07 23:46:47 +0700

    проверка на гильдию Чт 07 сен 2023 23:46:46 +07

--- roll.lua
+++ roll.lua
@@ -28,49 +28,52 @@ if string.find (message, "приглашается в вашу гильдию") 
 end
 
 if ginv ~= nil then
-	if string.find (message, "присоединяется к гильдии") then
-	local vName
-		for range,value in pairs(ginv) do
-			vName = value
-			if value == gInv[1] then
-				if testQ["shtrf"] == nil then
-					testQ["shtrf"] = {}
-				end
-				for range,value in pairs(testQ["shtrf"]) do
-					if value == gInv[1] then
-						shtraf = 1
-					else
-						shtraf = 0
+	local g,__,__ = GetGuildInfo("player")
+	if g == "Ночная стража" then
+		if string.find (message, "присоединяется к гильдии") then
+		local vName
+			for range,value in pairs(ginv) do
+				vName = value
+				if value == gInv[1] then
+					if testQ["shtrf"] == nil then
+						testQ["shtrf"] = {}
 					end
-				end
-				if shtraf == 0 or shtraf == nil then
-					if testQ[myNome]["zzlf"] ~= nil then
-						testQ[myNome]["zzlf"] = testQ[myNome]["zzlf"] + 1
-						SendAddonMessage("NSGadd", myNome .. " принял +1 ", "guild")
-						if testQ["mapQuestPodskazki"] ~= nil and testQ["mapQuestPodskazki"] ~= 1 then
-							testQ["mapQuestPodskazki"] = testQ["mapQuestPodskazki"] + 1
-						elseif testQ["mapQuestPodskazki"] == nil then
-							testQ["mapQuestPodskazki"] = 0
-						elseif testQ["mapQuestPodskazki"] >= 1 then
-							SendAddonMessage("#M1QP", myNome, "guild")
-							testQ["mapQuestPodskazki"] = nil
+					for range,value in pairs(testQ["shtrf"]) do
+						if value == gInv[1] then
+							shtraf = 1
+						else
+							shtraf = 0
 						end
-						if testQ[myNome]["настройки"]["roll"] == "Enable" then
-							local x = math.random(1, #gInvite)
-							SendChatMessage(gInvite[x], "WHISPER", nil, vName)
-						end
-						break
-					else
-						testQ[myNome]["zzlf"] = 1
 					end
-				else
-					SendAddonMessage("NSGadd", myNome .. " попытался наебать ником " .. gInv[1],"guild")
+					if shtraf == 0 or shtraf == nil then
+						if testQ[myNome]["zzlf"] ~= nil then
+							testQ[myNome]["zzlf"] = testQ[myNome]["zzlf"] + 1
+							SendAddonMessage("NSGadd", myNome .. " принял +1 ", "guild")
+							if testQ["mapQuestPodskazki"] ~= nil and testQ["mapQuestPodskazki"] ~= 1 then
+								testQ["mapQuestPodskazki"] = testQ["mapQuestPodskazki"] + 1
+							elseif testQ["mapQuestPodskazki"] == nil then
+								testQ["mapQuestPodskazki"] = 0
+							elseif testQ["mapQuestPodskazki"] >= 1 then
+								SendAddonMessage("#M1QP", myNome, "guild")
+								testQ["mapQuestPodskazki"] = nil
+							end
+							if testQ[myNome]["настройки"]["roll"] == "Enable" then
+								local x = math.random(1, #gInvite)
+								SendChatMessage(gInvite[x], "WHISPER", nil, vName)
+							end
+							break
+						else
+							testQ[myNome]["zzlf"] = 1
+						end
+					else
+						SendAddonMessage("NSGadd", myNome .. " попытался наебать ником " .. gInv[1],"guild")
+					end
 				end
 			end
+			table.insert(testQ["shtrf"], gInv[1])
+			shtraf = nil
 		end
-		table.insert(testQ["shtrf"], gInv[1])
-		shtraf = nil
-	end
+	else
 end
 
 end
