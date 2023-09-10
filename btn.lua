0e5f063c4726ae7586340f86ce5da5ead6b12ed2
Vladgobelen <vladgobelen@gmail.com>
2023-09-10 11:17:40 +0700

    Снижение нагрузки в 100500 раз. Карта Вс 10 сен 2023 11:17:40 +07

--- btn.lua
+++ btn.lua
@@ -1517,46 +1517,46 @@ frameTime:HookScript("OnUpdate", function(self, elapsed)
 		if krt ~= nil then
 			if krt["777"] == 2 then
 
-				if testQ["startChern"] == nil then
+				if krt["21"] == true and krt["21"] == true then
 					testFchern("evO0102",21,22)
 				end
-				if testQ["startChern"] == nil then
+				if krt["22"] == true and krt["23"] == true then
 					testFchern("evO0203",22,23)
 				end
-				if testQ["startChern"] == nil then
+				if krt["23"] == true and krt["24"] == true then
 					testFchern("evO0304",23,24)
 				end
-				if testQ["startChern"] == nil then
+				if krt["24"] == true and krt["25"] == true then
 					testFchern("evO0405",24,25)
 				end
-				if testQ["startChern"] == nil then
+				if krt["25"] == true and krt["26"] == true then
 					testFchern("evO0506",25,26)
 				end
-				if testQ["startChern"] == nil then
+				if krt["26"] == true and krt["27"] == true then
 					testFchern("evO0607",26,27)
 				end
-				if testQ["startChern"] == nil then
+				if krt["27"] == true and krt["28"] == true then
 					testFchern("evO0708",27,28)
 				end
-				if testQ["startChern"] == nil then
+				if krt["28"] == true and krt["29"] == true then
 					testFchern("evO0809",28,29)
 				end
-				if testQ["startChern"] == nil then
+				if krt["29"] == true and krt["30"] == true then
 					testFchern("evO0910",29,30)
 				end
-				if testQ["startChern"] == nil then
+				if krt["30"] == true and krt["31"] == true then
 					testFchern("evO1011",30,31)
 				end
-				if testQ["startChern"] == nil then
+				if krt["31"] == true then
 					testFchern("evO11",31,31)
 				end
-				if testQ["startChern"] == nil then
+				if krt["32"] == true then
 					testFchern("evO12",32,32)
 				end
-				if testQ["startChern"] == nil then
+				if krt["33"] == true then
 					testFchern("evO13",33,33)
 				end
-				if testQ["startChern"] == nil then
+				if krt["34"] == true then
 					testFchern("evO14",34,34)
 				end
 
