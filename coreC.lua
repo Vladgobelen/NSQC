78e1728219bc15da2be4cc46c0d6036e8e8fd023
Vladgobelen <vladgobelen@gmail.com>
2023-09-29 18:34:35 +0700

    первая версия для циркуля3 Пт 29 сен 2023 18:34:35 +07

--- coreC.lua
+++ coreC.lua
@@ -1,7 +1,6 @@
 local GC_Sniffer = CreateFrame("Frame")
 GC_Sniffer:RegisterEvent("CHAT_MSG_GUILD")
 GC_Sniffer:SetScript("OnEvent", function (self, event, message, sender)
-print(message,sender)
 --команды для управления квестами
 local nik=sender
 local msg = mysplit(message)
