5f62e6573e837bbb1e5495435125a6efa1928ded
Vladgobelen <vladgobelen@gmail.com>
2023-10-16 02:31:59 +0700

    Пн 16 окт 2023 02:31:59 +07

--- coreC.lua
+++ coreC.lua
@@ -32,6 +32,9 @@ if msg[1] == "\"трава" and msg[2] == myNome and testGM~=nil then
 	testQ["stog"] = tonumber(testQ["stog"])+tonumber(msg[3])
 	testQ["fRand6"] = 1
 end
+if msg[1] == myNome .. "," and msg[2] == "покажи" and msg[3] == "свой" then
+	SendChatMessage("У меня " .. testQ[myNome]["dTimer"], "OFFICER", nil, 1)
+end
 if msg[1] == "!добавить" and msg[2] == "предмет" and testGM~=nil then
 	local nazvanie
 	local n
