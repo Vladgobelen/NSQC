b26629f5de926e917e5769608bc55d7853220118
Vladgobelen <vladgobelen@gmail.com>
2025-04-20 20:38:03 +0700

    Вс 20 апр 2025 20:38:03 +07

---
+++ sounds.lua
@@ -0,0 +1,90 @@
+ns_alert_sounds = {
+    -- Системные звуки
+    {
+        label = "Системные",
+        sounds = {
+            -- Основные UI звуки
+            "Sound\\Interface\\AlarmClockWarning1.wav",
+            "Sound\\Interface\\AlarmClockWarning2.wav",
+            "Sound\\Interface\\AlarmClockWarning3.wav",
+            "Sound\\Interface\\AchievementMenuOpen.wav",
+            "Sound\\Interface\\AchievementMenuClose.wav",
+            "Sound\\Interface\\AuctionWindowOpen.wav",
+            "Sound\\Interface\\AuctionWindowClose.wav",
+            "Sound\\Interface\\MapPing.wav",
+            "Sound\\Interface\\ReadyCheck.wav",
+            "Sound\\Interface\\RaidWarning.wav",
+            "Sound\\Interface\\LevelUp.wav",
+            "Sound\\Interface\\LFG_RoleCheck.wav",
+            "Sound\\Interface\\LFG_Denied.wav",
+            "Sound\\Interface\\LFG_Rewards.wav",
+            "Sound\\Interface\\GuildVaultOpen.wav",
+            "Sound\\Interface\\GuildVaultClose.wav",
+            "Sound\\Interface\\TalentScreenOpen.wav",
+            "Sound\\Interface\\TalentScreenClose.wav",
+            
+            "Sound\\Spells\\SimonGame_Visual_GameStart.wav",
+            
+            -- Звуки способностей
+            "Sound\\Spells\\PVPEnterQueue.wav",
+            "Sound\\Spells\\PVPFlagTaken.wav",
+            "Sound\\Spells\\PVPFlagCaptured.wav",
+            "Sound\\Spells\\PVPFlagReturned.wav",
+            "Sound\\Spells\\ShaysBell.wav",
+            
+            -- Разные звуки
+            "Sound\\Doodad\\BellTollAlliance.wav",
+            "Sound\\Doodad\\BellTollHorde.wav",
+            "Sound\\Doodad\\BellTollNightElf.wav",
+            "Sound\\Doodad\\BoatDockedWarning.wav",
+            "Sound\\Doodad\\G_NecropolisWound.wav",
+            "Sound\\Doodad\\Goblin_Lottery_Open01.wav",
+            "Sound\\Doodad\\Goblin_Lottery_Open02.wav",
+            "Sound\\Doodad\\Goblin_Lottery_Open03.wav",
+            "Sound\\Doodad\\Goblin_Lottery_Open04.wav",
+            
+            -- Звуки событий
+            "Sound\\Event Sounds\\Event_wardrum_ogre.wav",
+            "Sound\\Event Sounds\\Wisp\\WispPissed1.wav",
+            "Sound\\Event Sounds\\Wisp\\WispPissed2.wav",
+            "Sound\\Event Sounds\\Wisp\\WispPissed3.wav",
+                        
+        },
+    },
+    -- Пользовательские звуки
+    {
+        label = "Пользовательские",
+        sounds = {
+		    "Interface\\AddOns\\NSQC\\libs\\fls.mp3",
+		    "Interface\\AddOns\\NSQC\\libs\\ach.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\bbbb.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\bt.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\clc.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\f.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\gob.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\gobXm.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\gom.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\hs.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\k.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\m.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\ms.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\mx.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\sh.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\smg.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\t.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\tr.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\tx.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\uz.ogg",
+		    "Interface\\AddOns\\NSQC\\libs\\kare.mp3",
+		    "Interface\\AddOns\\NSQC\\libs\\karg.mp3",
+		    "Interface\\AddOns\\NSQC\\libs\\karp.mp3",
+		    "Interface\\AddOns\\NSQC\\libs\\yaix.mp3",
+		    "Interface\\AddOns\\NSQC\\libs\\yaiz.mp3",
+		    "Interface\\AddOns\\NSQC\\fin.ogg",
+		    "Interface\\AddOns\\NSQC\\gob.ogg",
+		    "Interface\\AddOns\\NSQC\\lvlUp.ogg",
+		    "Interface\\AddOns\\NSQC\\punto.ogg",
+		    "Interface\\AddOns\\NSQC\\start.ogg",
+		}
+    },
+}
